1813f2a7c2cc73e0945b924a80b0689e29b4d041
askela2002 <44265688+askela2002@users.noreply.github.com>
2019-08-05 11:51:32 +0300

    Update polyfills.ts

--- todo/src/polyfills.ts
+++ todo/src/polyfills.ts
@@ -14,6 +14,7 @@
  * Learn more in https://angular.io/guide/browser-support
  */
 
+import 'core-js/es7/reflect';
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
